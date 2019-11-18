f4a0dc48cfb05621c15fac52c287c289084a16f3
Ben <boralben@gmail.com>
2019-11-17 19:58:22 -0600

    convenient and safe env selection

--- src/_util/axios-api.js
+++ src/_util/axios-api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://project-grand-canyon.appspot.com/api/'
-    // baseURL: 'http://localhost:8080/api/'
+    baseURL: process.env.REACT_APP_API_ENDPOINT || 'https://project-grand-canyon.appspot.com/api/'
 });
 
 export default instance;
