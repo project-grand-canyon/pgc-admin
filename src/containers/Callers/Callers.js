20b78c201596e9ded155c482a594a9c444ad1656
David Meyer <davidmey@umich.edu>
2020-08-06 06:08:08 -0400

    added column to All Callers Table for Super Admins (including sorter)

--- src/containers/Callers/Callers.js
+++ src/containers/Callers/Callers.js
@@ -13,14 +13,12 @@ import {
 } from "antd";
 import fileDownload from "js-file-download";
 
-import _ from "lodash";
-import {
-  getAllCallers,
-  getDistrictCallers,
-  getCallerHistories,
-} from "../../_util/axios-api";
-import { isSenatorDistrict } from "../../_util/district";
-import { Status, sortedByStatus, asCsv } from "../../_util/caller";
+import _ from 'lodash'
+import { DateTime } from 'luxon'
+import axios from '../../_util/axios-api';
+import { isSenatorDistrict, displayName } from '../../_util/district';
+import { callerStatus, Status, sortedByStatus } from '../../_util/caller';
+import { HistoryType } from './constants'
 
 import { connect } from "react-redux";
 
@@ -36,132 +34,112 @@ class Callers extends Component {
     generatingCsv: false,
   };
 
+  state = {
+    districtCallers: null,
+    allCallers: null,
+    callerDetail: {},
+    searchTerm: null
+  }
+
   isCallerInFocus = (key) => {
-    return (
-      this.state.callerDetail &&
-      this.state.callerDetail["caller"] &&
-      this.state.callerDetail["caller"]["callerId"] === key
-    );
-  };
+    return this.state.callerDetail && this.state.callerDetail['caller'] && this.state.callerDetail['caller']['callerId'] === key
+  }
 
   componentDidMount() {
     this.fetchCallers();
   }
 
-  columns = [
-    {
-      title: "First Name",
-      dataIndex: "firstName",
-      key: "firstName",
-      sorter: (a, b) => {
-        return a.firstName.localeCompare(b.firstName);
-      },
+  columns = [{
+    title: 'First Name',
+    dataIndex: 'firstName',
+    key: 'firstName',
+    sorter: (a, b) => { return a.firstName.localeCompare(b.firstName) }
+  }, {
+    title: 'Last Name',
+    dataIndex: 'lastName',
+    key: 'lastName',
+    sorter: (a, b) => { return a.lastName.localeCompare(b.lastName) }
+  }, {
+    title: 'District',
+    dataIndex: 'districtName',
+    key: 'districtName',
+    sorter: (a, b) => { return a.districtName.localeCompare(b.districtName) }
+  }, {
+    title: 'Call Day',
+    dataIndex: 'reminderDayOfMonth',
+    key: 'reminderDayOfMonth',
+    sorter: (a, b) => { return a.reminderDayOfMonth - b.reminderDayOfMonth }
+  }, {
+    title: 'Call Status',
+    dataIndex: 'status',
+    key: 'status',
+    render: (status) => {
+      return this.callStatusIcon(status);
     },
-    {
-      title: "Last Name",
-      dataIndex: "lastName",
-      key: "lastName",
-      sorter: (a, b) => {
-        return a.lastName.localeCompare(b.lastName);
-      },
-    },
-    {
-      title: "Call Day",
-      dataIndex: "reminderDayOfMonth",
-      key: "reminderDayOfMonth",
-      sorter: (a, b) => {
-        return a.reminderDayOfMonth - b.reminderDayOfMonth;
-      },
-    },
-    {
-      title: "Call Status",
-      dataIndex: "status",
-      key: "status",
-      render: (status) => {
-        return this.callStatusIcon(status);
-      },
-      sorter: sortedByStatus,
-    },
-    {
-      title: "Details",
-      dataIndex: "operation1",
-      fixed: "right",
-      width: 50,
-      render: (text, record) => {
-        return (
-          <div>
-            {this.isCallerInFocus(record.key) ? (
-              <Typography.Text>Details</Typography.Text>
-            ) : (
-              <Button onClick={() => this.showDetailModal(record.key)}>
-                Details
-              </Button>
+    sorter: sortedByStatus
+  }, {
+    title: 'Details',
+    dataIndex: 'operation1',
+    fixed: 'right',
+    width: 50,
+    render: (text, record) => {
+      return (
+        <div>
+          {this.isCallerInFocus(record.key) ? (
+            <Typography.Text>Details</Typography.Text>
+          ) : (
+              <Button onClick={() => this.showDetailModal(record.key)}>Details</Button>
             )}
-          </div>
-        );
-      },
+        </div>
+      );
     },
+  }
   ];
 
   callStatusIcon = ({ monthsMissedCount, status }) => {
     switch (status) {
-      case Status.CURRENT:
+      case (Status.CURRENT):
         return (
-          <Popover
-            content="This is an active caller."
-            title="Current"
-            trigger="hover"
-          >
+          <Popover content="This is an active caller." title="Current" trigger="hover">
             <Icon type="smile" theme="twoTone" twoToneColor="#52c41a" />
           </Popover>
-        );
-      case Status.BRAND_NEW:
+        )
+      case (Status.BRAND_NEW):
         return (
-          <Popover
-            content="This person hasn't been asked to call yet."
-            title="Brand New"
-            trigger="hover"
-          >
+          <Popover content="This person hasn't been asked to call yet." title="Brand New" trigger="hover">
             <Icon type="smile" theme="twoTone" />
           </Popover>
-        );
-      case Status.PAUSED:
+        )
+      case (Status.PAUSED):
         return (
-          <Popover
-            content="This person has paused call notifications. They are not participating in Project Grand Canyon."
+          <Popover content="This person has paused call notifications. They are not participating in Project Grand Canyon."
             title="Paused"
             trigger="hover"
           >
             <Icon type="pause-circle" theme="twoTone" twoToneColor="tan" />
           </Popover>
-        );
-      case Status.WAITING:
+        )
+      case (Status.WAITING):
         return (
-          <Popover
-            content="This person just recently got their notification, and they haven't made their call yet."
+          <Popover content="This person just recently got their notification, and they haven't made their call yet."
             title="Waiting For Call"
             trigger="hover"
           >
             <Icon type="message" theme="twoTone" twoToneColor="tan" />
           </Popover>
-        );
-      case Status.LAPSED:
+        )
+      case (Status.LAPSED):
         return (
-          <Popover
-            title="Lapsed"
-            content={`This person has not called for ${monthsMissedCount} ${
-              monthsMissedCount === 1 ? "month" : "months"
-            }`}
+          <Popover title="Lapsed"
+            content={`This person has not called for ${monthsMissedCount} ${monthsMissedCount === 1 ? "month" : "months"}`}
             trigger="hover"
           >
             <Icon type="phone" theme="twoTone" twoToneColor="red" />
             {monthsMissedCount > 1 && (
-              <Typography.Text type="danger">
-                &nbsp;&times;&nbsp;{monthsMissedCount}
-              </Typography.Text>
+              <Typography.Text type="danger">&nbsp;&times;&nbsp;{monthsMissedCount}</Typography.Text>
             )}
           </Popover>
-        );
+        )
       default:
         return (
           <>
@@ -170,89 +148,140 @@ class Callers extends Component {
           </>
         );
     }
-  };
+  }
 
   onUnfocusCaller = (cb) => {
     this.setState({ callerDetail: null }, () => {
-      cb && cb();
-    });
-  };
+      cb && cb()
+    })
+  }
 
   showDetailModal = (key) => {
-    const caller =
-      (this.state.districtCallers &&
-        this.state.districtCallers.find((el) => {
-          return el.key === key;
-        })) ||
-      (this.state.allCallers &&
-        this.state.allCallers.find((el) => {
-          return el.key === key;
-        }));
+    const caller = (this.state.districtCallers && this.state.districtCallers.find((el) => {
+      return el.key === key
+    })) || (this.state.allCallers && this.state.allCallers.find((el) => {
+      return el.key === key
+    }));
 
     if (caller) {
-      this.setState(
-        {
-          callerDetail: {
-            caller: caller,
-          },
-        },
-        () => {
-          getCallerHistories(
-            [this.state.callerDetail.caller],
-            (err, history) => {
-              const callerDetail = { ...this.state.callerDetail };
-              if (err) {
-                callerDetail.callReminderError = err.message;
-              } else {
-                callerDetail.history = this.makeTimeline(history[0]);
-              }
-              this.setState({ callerDetail });
-            }
-          );
+      this.setState({
+        callerDetail: {
+          caller: caller
         }
-      );
+      }, () => {
+        this.fetchCallerHistory();
+      });
     }
-  };
+  }
 
-  makeTimeline = (history) => {
-    const { signUpHistory, callHistory, reminderHistory } = {
-      ...history,
-    };
-    return _([])
-      .concat(signUpHistory, callHistory, reminderHistory)
-      .sortBy("timestamp")
-      .reverse()
-      .value();
-  };
-
-  fetchCallers = () => {
-    if (this.props.district) {
-      getDistrictCallers(this.props.district, (err, callers) => {
-        if (err) {
-          Modal.error({
-            title: "Error Loading Page",
-            content: err.message,
-          });
-          this.setState({ callerDetail: null });
-        } else {
-          this.setState({ districtCallers: callers, callerDetail: null });
-        }
+  fetchCallers() {
+    const district = this.props.district;
+    if (district) {
+      const requestOptions = {
+        url: `/callers?districtId=${district.districtId}`,
+        method: 'GET',
+        headers: { ...authHeader(), 'Content-Type': 'application/json' },
+      };
+      axios(requestOptions).then(({ data }) => {
+        // TODO: Possibly move this into reducer logic if Redux is implemented
+        const districtCallers = (data || []).map(caller => {
+          return {
+            ...caller,
+            key: caller.callerId,
+            contactMethodSMS: caller.contactMethods.indexOf('sms') !== -1,
+            contactMethodEmail: caller.contactMethods.indexOf('email') !== -1,
+            status: callerStatus(caller),
+          }
+        });
+        this.setState({ districtCallers, callerDetail: null });
+      }).catch((e) => {
+        Modal.error({
+          title: "Error Loading Page",
+          content: e.message,
+        });
+        this.setState({ callerDetail: null })
       });
     }
 
     if (this.props.user && this.props.user.root) {
-      getAllCallers((err, callers) => {
-        if (err) {
-          Modal.error({
-            title: "Error Loading Full Caller List",
-            content: err.message,
-          });
-        } else {
-          this.setState({ allCallers: callers });
-        }
+      const allCallerRequestOptions = {
+        url: `/callers`,
+        method: 'GET',
+        headers: { ...authHeader(), 'Content-Type': 'application/json' },
+      };
+      const allDistrictRequestOptions = {
+        url: `/districts`,
+        method: 'GET',
+        headers: { ...authHeader(), 'Content-Type': 'application/json' },
+      }
+      Promise.all([axios(allCallerRequestOptions), axios(allDistrictRequestOptions)]).then(([callers, districts]) => {
+        const allDistricts = new Map((districts.data || []).map(district => { return [district.districtId, district] }));
+        const allCallers = (callers.data || []).map(caller => {
+          return {
+            ...caller,
+            key: caller.callerId,
+            districtName: displayName(allDistricts.get(caller.districtId)),
+            contactMethodSMS: caller.contactMethods.indexOf('sms') !== -1,
+            contactMethodEmail: caller.contactMethods.indexOf('email') !== -1,
+            status: callerStatus(caller),
+          }
+        });
+        this.setState({ allCallers });
+      }).catch((e) => {
+        Modal.error({
+          title: "Error Loading Full Caller List",
+          content: e.message,
+        });
       });
     }
-  };
+  }
+
+  fetchCallerHistory() {
+    const district = this.props.district;
+    const caller = this.state.callerDetail && this.state.callerDetail.caller;
+    if (district && caller) {
+      const callHistoryRequestOptions = {
+        url: `/calls/${caller['callerId']}`,
+        method: 'GET',
+        headers: { ...authHeader(), 'Content-Type': 'application/json' }
+      };
+      const reminderHistoryRequestOptions = {
+        url: `/reminders/${caller['callerId']}`,
+        method: 'GET',
+        headers: { ...authHeader(), 'Content-Type': 'application/json' }
+      };
+      const callerDetail = { ...this.state.callerDetail };
+      Promise.all([
+        axios(callHistoryRequestOptions),
+        axios(reminderHistoryRequestOptions)
+      ]).then(([calls, reminders]) => {
+        // TODO: Possibly move this into reducer logic if Redux is implemented
+        const createHistoryItem = (timestamp, type) => {
+          const dateTime = DateTime.fromSQL(timestamp)
+
+          return {
+            timestamp: dateTime.valueOf(),
+            timestampDisplay: dateTime.toLocaleString(),
+            type,
+          }
+        }
+
+        const signUpHistory = [createHistoryItem(caller.created, HistoryType.SIGN_UP)]
+        const callHistory = _.map(calls.data, ({ created }) => createHistoryItem(created, HistoryType.CALL))
+        const reminderHistory = _.map(reminders.data, ({ timeSent }) => createHistoryItem(timeSent, HistoryType.NOTIFICATION))
+
+        callerDetail.history = _([])
+          .concat(signUpHistory, callHistory, reminderHistory)
+          .sortBy('timestamp')
+          .reverse()
+          .value()
+      }).catch(e => {
+        callerDetail.callReminderError = e.message;
+      }).then(() => {
+        this.setState({ callerDetail })
+      });
+    }
+  }
 
   componentDidUpdate(prevProps) {
     if (prevProps.district !== this.props.district) {
@@ -262,84 +291,39 @@ class Callers extends Component {
   }
 
   detailModal = () => {
-    const caller = this.state.callerDetail && this.state.callerDetail["caller"];
-    return (
-      <CallerDetailModal
-        caller={this.state.callerDetail}
-        display={caller != null}
-        onEditCaller={(callerDetails) => {
-          this.onEditCaller(callerDetails);
-        }}
-        onUnfocusCaller={this.onUnfocusCaller}
-        onSave={this.onSavedCaller}
-      ></CallerDetailModal>
-    );
-  };
+    const caller = this.state.callerDetail && this.state.callerDetail['caller']
+    return <CallerDetailModal
+      caller={this.state.callerDetail}
+      display={caller != null}
+      onEditCaller={(callerDetails) => { this.onEditCaller(callerDetails) }}
+      onUnfocusCaller={this.onUnfocusCaller}
+      onSave={this.onSavedCaller}
+    ></CallerDetailModal>
+  }
 
   onSavedCaller = () => {
     this.onUnfocusCaller(() => {
-      this.fetchCallers();
-    });
-  };
-
-  onClickDownloadAsCsv = (event) => {
-    if (!this.state.districtCallers) {
-      message.error("Could not generate a CSV");
-      return;
-    }
-
-    const hide = message.loading("Generating a CSV", 0);
-    getCallerHistories([...this.state.districtCallers], (err, histories) => {
-      hide();
-      if (err) {
-        message.error(`Could not generate a CSV - ${err.message}`);
-        return;
-      }
-
-      const enrichedCallers = this.state.districtCallers.map(
-        (caller, index) => {
-          const history = histories[index];
-          caller.paused = caller.paused ? "Paused" : "Active";
-          caller.totalCalls = history.callHistory.length;
-          caller.history = JSON.stringify(this.makeTimeline(history));
-          return caller;
-        }
-      );
-
-      const data = asCsv(enrichedCallers);
-      fileDownload(
-        data,
-        `${this.props.district.state}${this.props.district.number}.csv`
-      );
-      message.success(`CSV has downloaded!`);
-    });
-  };
+      this.fetchCallers()
+    })
+  }
 
   allCallersJsx = () => {
     if (this.props.user && this.props.user.root) {
-      const callers =
-        this.state.allCallers &&
-        this.state.allCallers.filter((el) => {
-          if (!this.state.searchTerm) {
-            return true;
-          }
-          return (
-            el.firstName.indexOf(this.state.searchTerm) !== -1 ||
-            el.lastName.indexOf(this.state.searchTerm) !== -1 ||
-            (el.email && el.email.indexOf(this.state.searchTerm) !== -1) ||
-            (el.phone && el.phone.indexOf(this.state.searchTerm) !== -1)
-          );
-        });
+      const callers = this.state.allCallers && this.state.allCallers.filter(el => {
+        if (!this.state.searchTerm) {
+          return true
+        }
+        return el.firstName.indexOf(this.state.searchTerm) !== -1 ||
+          el.lastName.indexOf(this.state.searchTerm) !== -1 ||
+          (el.email && el.email.indexOf(this.state.searchTerm) !== -1) ||
+          (el.phone && el.phone.indexOf(this.state.searchTerm) !== -1)
+      })
       return (
         <>
-          <Typography.Title level={2}>All Callers</Typography.Title>
-          <Input
-            allowClear
-            onChange={(e) => {
-              this.setState({ searchTerm: e.target.value });
-            }}
-            placeholder="Search by name, email, or phone number"
-          />
+          <Typography.Title level={2}>
+            All Callers
+          </Typography.Title>
+          <Input allowClear onChange={(e => { this.setState({ searchTerm: e.target.value }) })} placeholder="Search by name, email, or phone number" />
           <Table
             loading={this.state.allCallers === null}
             bordered
@@ -348,9 +332,7 @@ class Callers extends Component {
             scroll={{ x: 300 }}
             scrollToFirstRowOnChange
             pagination={{
-              onChange: (page) => {
-                this.onUnfocusCaller();
-              },
+              onChange: (page) => { this.onUnfocusCaller() },
             }}
           />
         </>
@@ -362,32 +344,26 @@ class Callers extends Component {
 
   render() {
     if (isSenatorDistrict(this.props.district)) {
-      return <Redirect to="/script" />;
+      return <Redirect to='/script' />;
     }
 
     return (
       <>
-        <Typography.Title level={2}>Callers for District</Typography.Title>
+        <Typography.Title level={2}>
+          Callers for District
+          </Typography.Title>
         {this.detailModal()}
-
-        <Button
-          disabled={this.state.districtCallers === null}
-          onClick={this.onClickDownloadAsCsv}
-        >
-          Download as CSV
-        </Button>
-
         <Table
           loading={this.state.districtCallers === null}
           bordered
           dataSource={this.state.districtCallers}
-          columns={this.columns}
+          columns={this.columns.filter(el => {
+            return el.key !== 'districtName';
+          })}
           scroll={{ x: 300 }}
           scrollToFirstRowOnChange
           pagination={{
-            onChange: (page) => {
-              this.onUnfocusCaller();
-            },
+            onChange: (page) => { this.onUnfocusCaller() },
           }}
         />
         {this.allCallersJsx()}
@@ -396,10 +372,10 @@ class Callers extends Component {
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     district: state.districts.selected,
-    user: state.admin.admin,
+    user: state.admin.admin
   };
 };
 
