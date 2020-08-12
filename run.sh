484e480ecf096a2aa97dc4a22ecf5b82aab05072
Aaron Sokoloski <aaron@mustardsystems.com>
2020-08-11 22:15:54 -0500

    Add run.sh as a place for convenient commands

---
+++ run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+export PATH=$PATH:node_modules/.bin
+
+start() {
+  react-app-rewired start
+}
+
+local() {
+  export REACT_APP_API_ENDPOINT=http://localhost:8080/api/
+  echo "Starting server, talking to API at $REACT_APP_API_ENDPOINT"
+  react-app-rewired start
+}
+
+build() {
+  react-app-rewired build
+}
+
+test() {
+  react-app-rewired test
+}
+
+"$@"
