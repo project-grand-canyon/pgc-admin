ae02514ad500fe9edef43f529847572563f0f694
Harry Chomsky <harry@chomsky.net>
2020-09-29 20:56:56 -0700

    Add senator reports on page with rep report

--- src/containers/Reports/Reports.js
+++ src/containers/Reports/Reports.js
@@ -1,10 +1,35 @@
 import React, { Component } from "react";
-import DistrictReport from "./DistrictReport.js";
+import { connect } from "react-redux";
+import { Divider } from "antd";
+import { isSenatorDistrict, comparator } from "../../_util/district";
+import DistrictReport from "./DistrictReport";
 
 class Reports extends Component {
   render() {
-    return <DistrictReport district={this.props.district} />;
+    const district = this.props.district;
+    const reportedDistricts = [];
+    if (district) {
+      reportedDistricts.push(district);
+    }
+    if (district && !isSenatorDistrict(district)) {
+      const senators = this.props.districts
+        .filter((el) => el.state == district.state && isSenatorDistrict(el))
+        .sort(comparator);
+      reportedDistricts.push(...senators);
+    }
+    return reportedDistricts.map((el, index) =>
+      <div key={el.districtId.toString()}>
+        {index > 0 && <Divider />}
+        <DistrictReport district={el} />
+      </div>
+    );
   }
 }
 
-export default Reports;
+const mapStateToProps = state => {
+  return {
+    districts: state.districts.districts
+  };
+};
+
+export default connect(mapStateToProps)(Reports);
